18f70e739dd8f97682cbfa4f05a4bb28f088b27f
Sergei Kuksov <ser.kuksov@mail.ru>
2023-04-06 20:57:11 +0300

    Подключен silk для мониторинга запросов к БД

--- web-app/config/settings.py
+++ web-app/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'silk',
+
     'plans',
     'accounts',
 ]
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
