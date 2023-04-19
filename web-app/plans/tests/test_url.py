327101af3bc0b59bf716a31350235dd220d556c5
Sergei Kuksov <ser.kuksov@mail.ru>
2023-04-19 19:37:18 +0300

    Тесты

--- web-app/plans/tests/test_url.py
+++ web-app/plans/tests/test_url.py
@@ -10,10 +10,11 @@ class UrlsTestCase(TestCase):
     def test_urls(self):
         self.assertEqual(resolve(reverse('plans:plan_list')).func.view_class, views.PlanListView)
         self.assertEqual(resolve(reverse('plans:task_list')).func.view_class, views.TaskListView)
-        self.assertEqual(resolve(reverse('plans:plan_create')).func.view_class, views.PlanCreateView)
-        self.assertEqual(resolve(reverse('plans:plan_detail', args=(1,))).func.view_class, views.PlanDetailView)
-        self.assertEqual(resolve(reverse('plans:plan_update', args=(1,))).func.view_class, views.PlanUpdateView)
-        self.assertEqual(resolve(reverse('plans:plan_delete', args=(1,))).func.view_class, views.PlanDeleteView)
+        self.assertEqual(resolve(reverse('plans:plan_create')).func.view_class, views.PlanAndTasksCreateView)
+        self.assertEqual(resolve(reverse('plans:plan_detail', args=(1,))).func.view_class, views.PlanAndTasksDetailView)
+        self.assertEqual(resolve(reverse('plans:plan_update', args=(1,))).func.view_class, views.PlanAndTasksUpdateView)
+        self.assertEqual(resolve(reverse('plans:plan_delete', args=(1,))).func.view_class, views.PlanAndTasksDeleteView)
         self.assertEqual(resolve(reverse('plans:task_detail', args=(1,))).func.view_class, views.TaskDetailView)
-        self.assertEqual(resolve(reverse('plans:create_word_doc_for_plan', args=(1,))).func, views.create_word_doc_for_plan_view)
+        self.assertEqual(resolve(reverse('plans:create_word_doc_for_plan', args=(1,))).func,
+                         views.create_word_doc_for_plan_view)
         self.assertEqual(resolve(reverse('plans:perfomer_update', args=(1,))).func.view_class, views.PerfomerUpdateView)
